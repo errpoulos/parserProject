ff87d68d8b13a9b217b11213f26b8196bf76abb5
errpoulos <erpoulos@unca.edu>
2018-02-13 16:26:14 -0500

    Fixed double printing problem.

--- src/src/TinyScanner.java
+++ src/src/TinyScanner.java
@@ -1,54 +1,86 @@
 package src;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PushbackReader;
-import java.io.Reader;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TinyScanner {
 
-    private final PushbackReader reader;
+    private class TokenDetails {
+        final Pattern regex;
+        final int token;
 
-    public TinyScanner(Reader reader) {
-        this.reader = new PushbackReader(reader);
+        /**
+         * Information about Tokens
+         * @param regex The pattern used to match input string for a token.
+         * @param token ID number given to each token group.
+         */
+        TokenDetails(Pattern regex, int token) {
+            super();
+            this.regex = regex;
+            this.token = token;
+        }
+    }
+
+    public class Token {
+        public final int token;
+        public final String sequence;
+
+        Token(int token, String sequence) {
+            super();
+            this.token = token;
+            this.sequence = sequence;
+        }
+    }
+
+    // Linked list of details about each input token
+    private LinkedList<TokenDetails> tokenDetails;
+    private LinkedList<Token> tokens;
+
+    TinyScanner() {
+        tokenDetails = new LinkedList<>();
+        tokens = new LinkedList<>();
     }
 
     /**
-     * Gets characters from input, matches them against regex patterns, returns the next token.
-     *
-     * @throws IOException
+     * Adds a regex pattern to validate a token from the input.
+     * @param regex The pattern to be matched.
+     * @param token The pattern's ID number.
      */
-    public void nextToken() throws IOException {
+    public void add(String regex, int token) {
+        tokenDetails.add(new TokenDetails(Pattern.compile("^(" + regex + ")"), token));
+    }
 
-        Tokenizer tokenizer = new Tokenizer();
-        //Logic ids
-        tokenizer.add("read|write|begin|end|if|then|else|endif|while|endwh", 1);
-        //Left Paren
-        tokenizer.add("\\(", 2);
-        //Right Paren
-        tokenizer.add("\\)", 3);
-        //Math Operators
-        tokenizer.add("\\+|-", 4);
-        tokenizer.add("\\*|/", 5);
-        //Assignment and Semicolon
-        tokenizer.add(":=", 6);
-        tokenizer.add(";", 7);
-        tokenizer.add("[0-9]+", 8);
-        tokenizer.add("[a-zA-Z][a-zA-Z0-9_]*", 8);
-        //Regex //A matches beginning of input
-        String input = new Scanner(new File("testFiles/testLeximes.txt")).useDelimiter("\\A").next();
-        System.out.println("Input: " + input);
-        try {
-            tokenizer.tokenize(input);
-            for (Tokenizer.Token tok : tokenizer.getTokens()) {
-                System.out.println("Token ID:" + tok.token + " Char or id:" + "(" + tok.sequence + ")");
+    /**
+     * Tokenizes a String.
+     * @param str String to be tokenized.
+     */
+    public void nextToken(String str) {
+        String s = str.trim();
+        tokens.clear();
+
+        while (!s.equals("")) {
+            boolean match = false;
+            for (TokenDetails details : tokenDetails) {
+                Matcher m = details.regex.matcher(s);
+                if (m.find()) {
+                    match = true;
+                    String tok = m.group().trim();
+                    tokens.add(new Token(details.token, tok));
+                    s = m.replaceFirst("").trim();
+                    tokens.add(new Token(details.token, tok));
+                    break;
+                }
             }
-        } catch (RuntimeException r) {
-            System.out.println(r.getMessage());
+            if (!match)
+                throw new RuntimeException("Invalid Character(s) found:\n" + s.charAt(0) + "\nremove from input before running again");
         }
     }
+    public LinkedList<Token> getTokens() {
+        return tokens;
+    }
 }
+
 
 
 
